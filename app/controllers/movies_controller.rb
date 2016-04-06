9537f4fe452f3c3c22c5063c56fe8382c3cd0dea
ROBEL TEKESTE <ROBEL.TEKESTE@GMAIL.COM>
2016-04-06 01:13:06 -0400

    rails index and shows functional with proper div separation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,10 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    respond_to do |format|
+      format.json{render json: @movies}
+      format.html
+    end
   end
 
   def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,7 @@ class Movie < ActiveRecord::Base
   belongs_to :director
   belongs_to :publisher
   has_many :actors
+
+  # scope :dc, where(:publisher_id => "dc")
+  # scope :marvel, where(:publisher_id => "marvel")
 end
